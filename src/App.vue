f8f4a45efed314aa59ca5822d5625a93bef0178e
saiyanLee <82094266+LeeViolet@users.noreply.github.com>
2025-08-13 11:56:43 +0000

    temp

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,32 @@ import { ref, provide } from "vue";
 
 import pals from "./pals.json";
 import BestMatch from "./BestMatch.vue";
+import NormalPair from "./NormalPair.vue";
 import Toast from "./components/Toast.vue";
 
 const toastRef = ref(null);
 provide("toast", toastRef);
+
+const tabs = [NormalPair, BestMatch];
+const tabIndex = ref(0);
+const tab = NormalPair;
 </script>
 
 <template>
     <div class="bg-sky-900 min-h-screen">
-        <BestMatch />
+        <div class="flex justify-center shadow-[0_4px_20px_rgba(0,0,0,0.1)] items-center space-x-8">
+            <div class="text-gray-200 px-8 py-6 transition-all duration-200 hover:bg-sky-800 cursor-pointer"
+                :class="{ 'bg-sky-700': tabIndex === 0 }" @click="tabIndex = 0">普通匹配</div>
+            <div class="text-gray-200 px-8 py-6 transition-all duration-200 hover:bg-sky-700 cursor-pointer"
+                :class="{ 'bg-sky-700': tabIndex === 1 }" @click="tabIndex = 1">路线匹配</div>
+        </div>
+        <Transition name="check" enter-active-class="transition-all duration-300 ease-out"
+            leave-active-class="transition-all duration-200 ease-in" enter-from-class="opacity-0"
+            leave-to-class="opacity-0">
+            <component :is="tabs[tabIndex]"></component>
+        </Transition>
         <Toast ref="toastRef" />
     </div>
 </template>
 
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-}
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/NormalPair.vue
@@ -0,0 +1,10 @@
+<script setup>
+import pals from "./data/pals.js";
+import combos from "./data/combos.js";
+</script>
+
+<template>
+    <div></div>
+</template>
+
+<style scoped></style>
